ae369b20b0fd4bd3e489543235937d03c8ad2b55
ADunbar5612 <157147898+ADunbar5612@users.noreply.github.com>
2026-02-04 14:22:20 -0600

    Succesfully uploaded the last push didnt work

--- CALC2000.cbl
+++ CALC2000.cbl
@@ -19,41 +19,37 @@
        01  user-entries.
 
            05  number-entered              pic 9        value 1.
-           05  investment-amount           pic 99999    value 1000.  
-           05  number-of-years             pic 99       value 10. 
+           05  investment-amount           pic 99999    value 1000.
+           05  number-of-years             pic 99       value 10.
            05  yearly-interest-rate        pic 99v9     value 5.5.
 
        01  work-fields.
 
            05  future-value                pic 9(7)v99.
            05  year-counter                pic 999.
-           
-           05 edited-whole-value           pic zz,zzz,zz9.
-           05 edited-decimal-value         pic zzz,zzz,99.
 
+           05  edited-whole-value           pic zz,zzz,zz9.    
+           05  edited-decimal-value         pic zzz,zzz.99.
+           
        procedure division.
 
        000-calculate-future-values.
 
            perform 100-calculate-future-value
-               until number-entered = zero.
+           COMPUTE investment-amount = investment-amount +
+            investment-amount
+           perform 100-calculate-future-value 
            display "End of session.".
            stop run.
 
        100-calculate-future-value.
-
-           display "----------------------------------------".
-           display "To end the program, enter 0.".
-           display "To perform another calculation, enter 1.".
-           display "----------------------------------------".
-           if number-entered = 1
                move investment-amount to future-value
                move 1 to year-counter
                perform 120-calculate-next-fv
                    until year-counter > number-of-years
                move future-value to edited-decimal-value
-               display "Future value = " edited-decimal-value.
-       
+             display "Calculating future value = " edited-decimal-value.
+
        120-calculate-next-fv.
 
            compute future-value rounded =
